e7bb1ab16a730b685ac5704e849b0db640873b64
Emad J. Maorun <emoun.open@gmail.com>
2017-05-23 17:58:21 +0200

    Moved Mutable and Graph into their own modules

---
+++ src/graph/mutable.rs
@@ -0,0 +1,36 @@
+
+use graph::FineGrainedGraph;
+
+pub trait GraphBinding<G>{
+	
+	fn graph(self) -> G;
+}
+
+pub trait CollectorBinding<V,G> : GraphBinding<G>{
+	
+	fn iter(&self) -> Iterator<Item=(V)>;
+	
+}
+
+pub trait Mutable<'a,G,V,E,O,I,Vc,Ec,Oc,Ic>: FineGrainedGraph<'a,
+	Vertex				=	V,
+	VertexCollector		=	Vc,
+	EdgeCollector 		=  	Ec,
+	OutgoingCollector 	=	Oc,
+	IncomingCollector 	=	Ic,
+>
+	where
+		G:	Mutable<'a,G,V,E,O,I,Vc,Ec,Oc,Ic>,
+		V: 	Clone,
+		Vc:	CollectorBinding<V,G>,
+		Ec:	CollectorBinding<E,G>,
+		Oc:	CollectorBinding<O,G>,
+		Ec:	CollectorBinding<I,G>,
+{
+	fn get_mutable(self) -> G;
+	
+	fn get_immutable(self) -> Self;
+	
+	fn add_vertex(self, v: V) -> G;
+	
+}
