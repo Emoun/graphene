5087299c19a7884a6c08e57df85b3ca4b837b31b
Emad Jacob Maroun <emoun.open@gmail.com>
2018-04-12 20:05:13 +0200

    Added dependency on dmutil.

--- tests/all_tests.rs
+++ tests/all_tests.rs
@@ -1,10 +1,14 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
+//#![feature(trace_macros)] //trace_macros!(true);
 #[macro_use]
 extern crate graphene;
 #[cfg(test)]
 #[macro_use]
 extern crate quickcheck;
+#[macro_use]
+extern crate dmutil;
+
 
 #[macro_use]
 mod arbitraries;
@@ -14,4 +18,3 @@ mod core;
 use graphene::core::*;
 use graphene::core::constraint::*;
 use graphene::common::*;
-
