4fcf52e93189ba3a705b759b91ceb9c3f7a090cc
Emad Jacob Maroun <emoun.open@gmail.com>
2019-10-02 18:23:07 +0200

    Added associated type to test trait.

--- tests/test.rs
+++ tests/test.rs
@@ -1,13 +1,14 @@
 use std::ops::{Deref, DerefMut};
 
 trait Graph {
-	fn graph_fn(&self) -> u32 ;
+	type R: PartialOrd<u32>;
+	fn graph_fn(&self) -> Self::R ;
 }
 trait GraphMut: Graph {
-	fn graph_mut(&mut self) -> &mut u32;
+	fn graph_mut(&mut self) -> &mut Self::R;
 }
 trait GraphMut2: Graph {
-	fn graph_mut2(&mut self) -> &mut u32;
+	fn graph_mut2(&mut self) -> &mut Self::R;
 }
 
 trait BaseConstraint: Sized {
@@ -19,12 +20,10 @@ trait BaseConstraint: Sized {
 		G::constrain(self)
 	}
 }
-trait BaseConstraintMut: BaseConstraint
-{
+trait BaseConstraintMut: BaseConstraint {
 	fn get_graph_mut(&mut self) -> &mut Self::Graph;
 }
-trait Constraint: Sized
-{
+trait Constraint: Sized {
 	type Base:  BaseConstraint;
 	type Constrained: Constraint<Base=Self::Base>;
 	
@@ -43,33 +42,32 @@ trait Constraint: Sized
 	}
 	
 }
-trait ConstraintMut: Constraint
-{
+trait ConstraintMut: Constraint<Base=<Self as ConstraintMut>::BaseMut, Constrained=<Self as ConstraintMut>::ConstrainedMut> {
 	type BaseMut:  BaseConstraintMut;
 	type ConstrainedMut: ConstraintMut<BaseMut=Self::BaseMut>;
+	
 	fn base_single_mut(&mut self) -> &mut Self::ConstrainedMut;
-	fn base_mut(&mut self) -> &mut <Self::BaseMut as BaseConstraint>::Graph {
+	fn base_mut(&mut self) -> &mut <Self::BaseMut as BaseConstraint>::Graph
+	{
 		self.base_single_mut().base_mut()
 	}
 }
 
-impl<G: Graph, D: Deref<Target=G>> BaseConstraint for D
-{
+impl<G: Graph, D: Deref<Target=G>> BaseConstraint for D {
 	type Graph = G;
 	fn get_graph(&self) -> &Self::Graph {&**self}
 }
-impl<G: GraphMut, D: DerefMut<Target=G>> BaseConstraintMut for D
-{
+impl<G: GraphMut, D: DerefMut<Target=G>> BaseConstraintMut for D {
 	fn get_graph_mut(&mut self) -> &mut Self::Graph {&mut **self}
 }
 impl<B: BaseConstraint> Constraint for B {
 	type Base = Self;
 	type Constrained = Self;
-	
+
 	fn base_single(&self) -> &Self::Constrained { &self }
 	fn unconstrain_single(self) -> Self::Constrained { self }
 	fn constrain_single(g: Self::Constrained) -> Result<Self, ()> { Ok(g)}
-	
+
 	fn unconstrain(self) -> Self::Base { self }
 	fn constrain(g: Self::Base) -> Result<Self, ()>{ Ok(g) }
 	fn base(&self) -> &<Self::Base as BaseConstraint>::Graph { self.get_graph() }
@@ -77,7 +75,7 @@ impl<B: BaseConstraint> Constraint for B {
 impl<B: BaseConstraintMut> ConstraintMut for B {
 	type BaseMut = Self;
 	type ConstrainedMut = Self;
-	fn base_single_mut(&mut self) -> &mut Self::Constrained { self }
+	fn base_single_mut(&mut self) -> &mut Self::ConstrainedMut { self }
 	fn base_mut(&mut self) -> &mut <Self::BaseMut as BaseConstraint>::Graph {
 		self.get_graph_mut()
 	}
@@ -91,6 +89,7 @@ impl BaseGraph {
 	}
 }
 impl Graph for BaseGraph{
+	type R = u32;
 	fn graph_fn(&self) -> u32 {
 		self.0
 	}
@@ -110,30 +109,7 @@ impl BaseConstraintMut for BaseGraph{
 }
 
 struct Connected<C: Constraint>(C);
-impl<C: Constraint> Connected<C>
-{
-	fn connected_fn(&self) -> u32
-	{
-		self.base().graph_fn()
-	}
-}
-impl<C: Constraint> Graph for Connected<C>{
-	fn graph_fn(&self) -> u32 {
-		self.base().graph_fn()
-	}
-}
-impl<C: ConstraintMut> GraphMut for Connected<C>
-	where <C::BaseMut as BaseConstraint>::Graph: GraphMut
-{
-	fn graph_mut(&mut self) -> &mut u32 { self.0.base_mut().graph_mut() }
-}
-impl<C: ConstraintMut> GraphMut2 for Connected<C>
-	where <C::BaseMut as BaseConstraint>::Graph: GraphMut2
-{
-	fn graph_mut2(&mut self) -> &mut u32 { self.0.base_mut().graph_mut2() }
-}
-impl<C: Constraint> Constraint for Connected<C>
-{
+impl<C: Constraint> Constraint for Connected<C> {
 	type Base = C::Base;
 	type Constrained = C;
 
@@ -148,12 +124,35 @@ impl<C: Constraint> Constraint for Connected<C>
 
 	}
 }
-impl<C: ConstraintMut> ConstraintMut for Connected<C>
-{
+impl<C: ConstraintMut> ConstraintMut for Connected<C> {
 	type BaseMut = C::BaseMut;
 	type ConstrainedMut = C;
 	
-	fn base_single_mut(&mut self) -> &mut Self::ConstrainedMut { &mut self.0 }
+	fn base_single_mut(&mut self) -> &mut Self::Constrained { &mut self.0 }
+}
+
+impl<C: Constraint> Connected<C> {
+	fn connected_fn(&self) -> <<C::Base as BaseConstraint>::Graph as Graph>::R
+	{
+		self.base().graph_fn()
+	}
+}
+impl<C: Constraint> Graph for Connected<C>{
+	type R = <<C::Base as BaseConstraint>::Graph as Graph>::R;
+	fn graph_fn(&self) -> Self::R {
+		self.base().graph_fn()
+	}
+}
+impl<C: ConstraintMut> GraphMut for Connected<C>
+	where <C::Base as BaseConstraint>::Graph: GraphMut {
+	fn graph_mut(&mut self) -> &mut <<C::Base as BaseConstraint>::Graph as Graph>::R
+	{
+		self.0.base_mut().graph_mut()
+	}
+}
+impl<C: ConstraintMut> GraphMut2 for Connected<C>
+	where <C::Base as BaseConstraint>::Graph: GraphMut2 {
+	fn graph_mut2(&mut self) -> &mut <<C::Base as BaseConstraint>::Graph as Graph>::R { self.0.base_mut().graph_mut2() }
 }
 
 #[test]
@@ -163,65 +162,65 @@ fn constrainer_constraining_base(){
 		Connected<
 			&'a BaseGraph
 		>>;
-	
+
 	let mut g = BaseGraph(16);
 	assert_eq!(g.graph_fn(), 16);
-	
+
 	let c_ref = ConstrainedGraphRef::constrain(&g).unwrap();
 	assert_eq!(c_ref.connected_fn(), 16);
-	
+
 	type ConstrainedGraphMut<'a> =
 	Connected<
 		Connected<
 			&'a mut BaseGraph
 		>>;
-	
+
 	let mut c_ref_mut = ConstrainedGraphMut::constrain(&mut g).unwrap();
 	*c_ref_mut.graph_mut() = 30;
 	*c_ref_mut.graph_mut2() = 30;
 	assert_eq!(c_ref_mut.connected_fn(), 30);
-	
+
 	type ConstrainedGraph<'a> =
 	Connected<
 		Connected<
 			BaseGraph
 		>>;
-	
+
 	let c_owned = ConstrainedGraph::constrain(g).unwrap();
 	assert_eq!(c_owned.connected_fn(), 30);
-	
+
 }
 
 #[test]
 fn inline_constrainer_constraining_base(){
-	
+
 	let mut g = BaseGraph(16);
 	assert_eq!(g.graph_fn(), 16);
-	
+
 	let c_ref = <Connected<Connected<&BaseGraph>>>::constrain(&g).unwrap();
 	assert_eq!(c_ref.connected_fn(), 16);
-	
+
 	let mut c_ref_mut = <Connected<Connected<&mut BaseGraph>>>::constrain(&mut g).unwrap();
 	*c_ref_mut.graph_mut() = 30;
 	assert_eq!(c_ref_mut.connected_fn(), 30);
-	
+
 	let c_owned = <Connected<Connected<BaseGraph>>>::constrain(g).unwrap();
 	assert_eq!(c_owned.connected_fn(), 30);
-	
+
 }
 
 #[test]
 fn base_constrains_self_by_constraint_inference(){
 	type ConstrainedGraph<G> = Connected< Connected< Connected<G> > >;
-	
+
 	let mut g = BaseGraph(16);
 	assert_eq!(g.graph_fn(), 16);
-	
+
 	let c_ref: ConstrainedGraph<&BaseGraph> = (&g).constrain().unwrap();
 	assert_eq!(c_ref.connected_fn(), 16);
 	let c_ref_unc = c_ref.unconstrain_single();
 	assert_eq!(c_ref_unc.connected_fn(), 16);
-	
+
 	let mut c_ref_mut: ConstrainedGraph<&mut BaseGraph> = (&mut g).constrain().unwrap();
 	*c_ref_mut.graph_mut() = 30;
 	assert_eq!(c_ref_mut.connected_fn(), 30);
@@ -229,7 +228,7 @@ fn base_constrains_self_by_constraint_inference(){
 	assert_eq!(c_ref_mut_unc.connected_fn(), 30);
 	*c_ref_mut_unc.graph_mut() = 31;
 	assert_eq!(c_ref_mut_unc.connected_fn(), 31);
-	
+
 	let c_owned: ConstrainedGraph<BaseGraph> = g.constrain().unwrap();
 	assert_eq!(c_owned.connected_fn(), 31);
 	let c_owned_unc = c_owned.unconstrain_single();
@@ -240,15 +239,15 @@ fn base_constrains_self_by_constraint_inference(){
 
 #[test]
 fn base_constrains_self_by_inline_constraint_inference(){
-	
+
 	let mut g = BaseGraph(16);
 	assert_eq!(g.graph_fn(), 16);
-	
+
 	let c_ref: Connected<Connected<Connected<&BaseGraph>>> = (&g).constrain().unwrap();
 	assert_eq!(c_ref.connected_fn(), 16);
 	let c_ref_unc = c_ref.unconstrain_single();
 	assert_eq!(c_ref_unc.connected_fn(), 16);
-	
+
 	let mut c_ref_mut: Connected<Connected<Connected<&mut BaseGraph>>> = (&mut g).constrain().unwrap();
 	*c_ref_mut.graph_mut() = 30;
 	assert_eq!(c_ref_mut.connected_fn(), 30);
@@ -256,7 +255,7 @@ fn base_constrains_self_by_inline_constraint_inference(){
 	assert_eq!(c_ref_mut_unc.connected_fn(), 30);
 	*c_ref_mut_unc.graph_mut() = 31;
 	assert_eq!(c_ref_mut_unc.connected_fn(), 31);
-	
+
 	let c_owned: Connected<Connected<Connected<BaseGraph>>> = g.constrain().unwrap();
 	assert_eq!(c_owned.connected_fn(), 31);
 	let c_owned_unc = c_owned.unconstrain_single();
